20a8ae166755590cd46a057db0b1020147a535f3
LSJ-90 <lsj7641148@naver.com>
2022-05-03 15:08:27 +0900

    20220503

--- study/javascript/practice.js
+++ study/javascript/practice.js
@@ -1,7 +1,13 @@
-const arr = [1,2,3];
+// const arr = [1,2,3];
+// arr.forEach(console.log);
+// console.log("arr배열 타입: " + typeof arr);
+// console.log("arr배열 크기: " + arr.length);
 
-arr.forEach(console.log);
+// const arr2 = {name: "이승준", age: 38};
+// console.log(arr2.name)
+// console.log(typeof arr2);
 
-const arr2 = {name: "이승준", age: 38};
+// 이스케이프
+var str = "안녕하수꽝~ \n이승준이수다~";
+console.log(str);
 
-console.log(arr2.name);
